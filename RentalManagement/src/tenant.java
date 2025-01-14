86e8a8d17244916373bd36d49bbe0df60f561ed0
Tranthuy03 <thuy.tranthi.cit21@eiu.edu.vn>
2025-01-14 21:53:56 +0700

    Update tenant.java

--- RentalManagement/src/tenant.java
+++ RentalManagement/src/tenant.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
 
-/**
- *
- * @author LENOVO
- */
 public class tenant {
 
     /**
