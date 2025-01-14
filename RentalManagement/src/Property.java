3bbe28365251265285ab1525f9222b32670f5011
Tranthuy03 <thuy.tranthi.cit21@eiu.edu.vn>
2025-01-14 22:32:29 +0700

    Update Property.java

--- RentalManagement/src/Property.java
+++ RentalManagement/src/Property.java
@@ -1,7 +1,35 @@
 public class Property {
-    int PropertyId;
-	String description;
-	int PropertyOwnerID;
-	
-    
+    private String propertyName;
+    private String propertyAddress;
+    private double propertyValue;
+
+    public Property(String propertyName, String propertyAddress, double propertyValue) {
+        this.propertyName = propertyName;
+        this.propertyAddress = propertyAddress;
+        this.propertyValue = propertyValue;
+    }
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    public void setPropertyName(String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    public String getPropertyAddress() {
+        return propertyAddress;
+    }
+
+    public void setPropertyAddress(String propertyAddress) {
+        this.propertyAddress = propertyAddress;
+    }
+
+    public double getPropertyValue() {
+        return propertyValue;
+    }
+
+    public void setPropertyValue(double propertyValue) {
+        this.propertyValue = propertyValue;
+    }
+
 }
